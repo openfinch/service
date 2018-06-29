3d43d6371be3a9df577f5dee4901e15944a86193
Josh Finch <me@joshfinch.com>
2018-06-29 10:19:49 +0100

    Update service.py

--- service.py
+++ service.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, Josh Finch.
+#
+# This file is part of service.
+#
+# service is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# service is based on code published as part of charm-helpers, copyright
+# Canonical Ltd and published under the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation.
+#
+# service is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with service.  If not, see <http://www.gnu.org/licenses/>.
+
 import os, subprocess
 
 def start(name):
